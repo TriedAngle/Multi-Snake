8b8b5fcdc713d2964ee0a32114fb08687f925b98
Sebastian Strobl <Sebastian@Strobl.net>
2019-07-27 19:03:22 +0200

    : ^)

---
+++ game/objects/tile.py
@@ -0,0 +1,17 @@
+class Tile:
+    tile_id = None
+    tile_x = None
+    tile_y = None
+    tile_width = 16
+    tile_height = 16
+
+    def render(self):
+        pass
+
+    def update(self):
+        pass
+
+
+class Dirt(Tile):
+    pass
+
