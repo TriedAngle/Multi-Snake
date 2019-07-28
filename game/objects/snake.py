90d9f7ecfcf5a03433b9a3e78667c3b4c4e547df
Sebastian Strobl <Sebastian@Strobl.net>
2019-07-28 16:47:46 +0200

    fail XD

--- game/objects/snake.py
+++ game/objects/snake.py
@@ -5,17 +5,23 @@ from game.utility.assets import asset
 class Snake:
     x = None
     y = None
+    snake_parts = []
     width = 64
     height = 64
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.snake_parts.append((x, y))
         self.tick = 0
         self.mov_locked = False
 
     def render(self, window):
-        window.blit(asset.get_sprite(0), (self.x, self.y))
+        for part in self.snake_parts:
+            if part == self.snake_parts[0]:
+                window.blit(asset.get_sprite(0), part)
+            else:
+                window.blit(asset.get_sprite(1), part)
 
     def update(self):
         self.tick += 1
@@ -37,3 +43,5 @@ class Snake:
             if key[pygame.K_d]:
                 self.x += 64
                 self.mov_locked = True
+
+        self.snake_parts[0] = self.x, self.y
